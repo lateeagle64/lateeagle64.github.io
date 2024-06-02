acfa54ebf23e8acc65b5d24c9ffe7de5bc913a7a
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-06-02 13:43:36 -0400

    Update games.js

--- games.js
+++ games.js
@@ -5,6 +5,10 @@
       "url": "https://chromedino.com"
     },
     {
+      "name": "Ovo",
+      "url": "https://www.coolmathgames.com/0-ovo"
+    },
+    {
       "name": "Super Mario Bros",
       "url": "https://90kids.com/mario/"
     },
@@ -17,9 +21,22 @@
       "url": "https://90kids.com/street-fighter-2/"
     },
     {
-      "name": "Ovo",
-      "url": "https://www.coolmathgames.com/0-ovo"
-    }
+      "name": "Run",
+      "url": "https://www.coolmathgames.com/0-run"
+    },
+    {
+      "name": "Run 2",
+      "url": "https://www.coolmathgames.com/0-run-2"
+    },
+    {
+      "name": "Run 3",
+      "url": "https://www.coolmathgames.com/0-run-3"
+    },
+    {
+      "name": "Paper.io 2",
+      "url": "https://paperio-2.github.io/"
+    },
   ]
 }
 
+https://slope3d.io/
