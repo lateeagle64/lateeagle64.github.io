3dd61d12827bda76cddb50f06447590f578f6058
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-10-12 18:54:59 -0400

    Update marquee.js

--- home/marquee.js
+++ home/marquee.js
@@ -6,9 +6,6 @@ const marquee = [
 
 // Select a random message from the array
 const randomMessage = marquee[Math.floor(Math.random() * marquee.length)];
-
-// Repeat the message 10 times, with a space after each one except the last
 const repeatedMessage = Array(10).fill(randomMessage).join(' ');
 
-// Update the marquee content
 document.getElementById("marquee-text").textContent = repeatedMessage;
