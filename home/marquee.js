238d90e296a9b67d3621f13ca4ec40e16613196d
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-10-13 17:51:52 -0400

    Update marquee.js

--- home/marquee.js
+++ home/marquee.js
@@ -5,6 +5,6 @@ const marquee = [
 ];
 
 const randomMessage = marquee[Math.floor(Math.random() * marquee.length)];
-const messageSpacer = Array(10).fill(randomMessage).join(' - ');
+const messageSpacer = Array(20).fill(randomMessage).join(' - ');
 
 document.getElementById("marquee-text").textContent = messageSpacer;
