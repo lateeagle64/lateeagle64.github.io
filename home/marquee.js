30908db0b5f08f927fa335dc115de9330f10ccce
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-10-13 17:53:37 -0400

    Update marquee.js

--- home/marquee.js
+++ home/marquee.js
@@ -2,6 +2,7 @@ const marquee = [
     "lateeagle64.github.io",
     "made by lateeagle64 on github",
     "thanks for using my website",
+    "share this website to others",
 ];
 
 const randomMessage = marquee[Math.floor(Math.random() * marquee.length)];
