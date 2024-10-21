dadde6b935d7ce957c3b07100a28172350d7ed5d
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-10-21 07:15:48 -0400

    Update proxies.js

--- proxies/proxies.js
+++ proxies/proxies.js
@@ -1,6 +1,7 @@
 const proxies = [
   {
     name: "interstellar",
+    image: "https://lateeagle64.github.io/images/interstellar.png/"
     urls: ["https://welcome.chillat.net/", "https://social.valah.net/", "https://services.nexus-computing.com/"]
   },
 ];
