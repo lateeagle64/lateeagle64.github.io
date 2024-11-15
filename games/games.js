c2732c05e7c496105ca75961443107ff31d0ebfc
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2024-11-14 20:31:54 -0500

    Create games.js

---
+++ games/games.js
@@ -0,0 +1,8 @@
+const games = [
+  {
+    name: "Minecraft",
+    image: "https://lateeagle64.github.io/images/minecraftImage.png",
+    selectImage: "https://lateeagle64.github.io/images/minecraftImageSelected.png",
+    url: "https://lateeagle64.github.io/minecraft",
+  },
+]
