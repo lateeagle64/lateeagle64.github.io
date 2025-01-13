590badc5786f427ece3ea02b0bf051494a401742
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2025-01-13 16:01:15 -0500

    Delete games directory

--- games/games.js
+++
@@ -1,8 +0,0 @@
-const games = [
-  {
-    name: "Minecraft",
-    image: "https://lateeagle64.github.io/images/minecraftImage.png",
-    selectImage: "https://lateeagle64.github.io/images/minecraftImageSelected.png",
-    url: "https://lateeagle64.github.io/minecraft",
-  },
-]
