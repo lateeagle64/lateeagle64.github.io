9f8497dc86dc79aa4a7714d15e5ea5058944be63
lateeagle64 <144566293+lateeagle64@users.noreply.github.com>
2025-11-20 13:39:23 -0500

    Update game.js

--- drum-legends/scripts/game.js
+++ drum-legends/scripts/game.js
@@ -1,3 +1,37 @@
-let binds = {
-  
+let mainBinds = {
+  "1": "D",
+  "2": "F",
+  "3": "J",
+  "4": "K",
 };
+
+// 1 - Bass Drum + Floor Tom + Ride Cymbal
+// 2 - Snare + Hi-Tom
+// 3 - Hi-Hat + Medium Tom
+// 4 - Crash Symbal
+
+// 11/20/25, no idea how I'll make this
+
+// Multiple different sounds combined so you aren't pressing 8 different keys
+// You can use 8 keys when playing freeplay, and if I add a recording system for custom songs
+// I might make it so you can upload the actual song without drums tho
+
+let freeplayBinds = {
+  "1": "D",
+  "2": "F",
+  "3": "J",
+  "4": "K",
+  "5": "C",
+  "6": "V",
+  "7": "N",
+  "8": "M",
+};
+
+// 1 - Bass Drum
+// 2 - Snare
+// 3 - Hi-Hat
+// 4 - Crash Symbal
+// 5 - Floor Tom
+// 6 - Hi-Tom
+// 7 - Medium Tom
+// 8 - Ride Cymbal
